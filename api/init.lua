38a0e55e904cac947b8d1acbe8ba8542f89f4ecd
bjorn <tie.372@gmail.com>
2018-02-18 19:37:51 -0800

    Fix World;

--- api/init.lua
+++ api/init.lua
@@ -13266,17 +13266,17 @@ return {
             {
               name = "Colliders",
               tag = "colliders",
-              description = "        The following functions add Colliders to the World.  `World:newCollider` adds an \"empty\"\n        Collider without any Shapes attached, whereas the other functions are shortcut functions to\n        add Colliders with Shapes already attached to them.\n      "
+              description = "The following functions add Colliders to the World.  `World:newCollider` adds an \"empty\" Collider without any Shapes attached, whereas the other functions are shortcut functions to add Colliders with Shapes already attached to them."
             },
             {
               name = "Properties",
               tag = "worldProperties",
-              description = "        The following functions are global properties of the simulation that apply to all new\n        Colliders.\n      "
+              description = "The following functions are global properties of the simulation that apply to all new Colliders."
             },
             {
               name = "Collision",
               tag = "worldCollision",
-              description = "        When the World is created using `lovr.physics.newWorld`, it is possible to specify a list of\n        collision tags for the World.  Colliders can then be assigned a tag.  You can enable and\n        disable collision between pairs of tags.  There are also some helper functions to quickly\n        identify pairs of colliders that are near each other and test whether or not they are\n        colliding.  These are used internally by default by `World:update`, but you can override\n        this behavior and use the functions directly for custom collision behavior.\n      "
+              description = "When the World is created using `lovr.physics.newWorld`, it is possible to specify a list of collision tags for the World.  Colliders can then be assigned a tag.  You can enable and disable collision between pairs of tags.  There are also some helper functions to quickly identify pairs of colliders that are near each other and test whether or not they are colliding.  These are used internally by default by `World:update`, but you can override this behavior and use the functions directly for custom collision behavior."
             }
           },
           notes = "Be sure to update the World in `lovr.update` using `World:update`, otherwise everything will stand still.",
