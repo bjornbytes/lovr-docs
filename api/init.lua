d9cbbb4f384dd25dbc24adf4aa054a616582342f
bjorn <tie.372@gmail.com>
2018-03-17 10:13:58 -0700

    Add texture variants to newModel;
    (cherry picked from commit f108031ce6e09acda9eaf79ff6aa715ce5d90d9d)

--- api/init.lua
+++ api/init.lua
@@ -5086,6 +5086,7 @@ return {
           description = "Creates a new Model from a file.  The supported 3D file formats are `obj`, `fbx`, `gltf`, and collada.  Models use normals and texture coordinates, if provided.\n\nThe following features are not supported yet: animations, materials, vertex colors.",
           key = "lovr.graphics.newModel",
           module = "lovr.graphics",
+          notes = "Models loaded from glTF files do not currently import animations properly.",
           variants = {
             {
               arguments = {
@@ -5093,6 +5094,34 @@ return {
                   name = "filename",
                   type = "string",
                   description = "The filename of the model to load."
+                },
+                {
+                  name = "texture",
+                  type = "string",
+                  description = "The filename of the texture to apply to the model.",
+                  default = "nil"
+                }
+              },
+              returns = {
+                {
+                  name = "model",
+                  type = "Model",
+                  description = "The new Model."
+                }
+              }
+            },
+            {
+              arguments = {
+                {
+                  name = "filename",
+                  type = "string",
+                  description = "The filename of the model to load."
+                },
+                {
+                  name = "material",
+                  type = "Material",
+                  description = "The material to apply to the model.  If nil, the materials will be loaded from the model file.",
+                  default = "nil"
                 }
               },
               returns = {
@@ -5103,8 +5132,7 @@ return {
                 }
               }
             }
-          },
-          notes = "Models loaded from glTF files do not currently import animations properly."
+          }
         },
         {
           name = "newShader",
