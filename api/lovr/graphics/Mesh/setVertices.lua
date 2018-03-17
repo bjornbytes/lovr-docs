c724bf69cd18fca8cca5c9f70354334d35e1aa6e
bjorn <tie.372@gmail.com>
2018-03-17 10:27:31 -0700

    Add start argument to Mesh:setVertices;
    (cherry picked from commit ae496897f11c2f761164b54b54b95de7cd8281e6)

--- api/lovr/graphics/Mesh/setVertices.lua
+++ api/lovr/graphics/Mesh/setVertices.lua
@@ -6,10 +6,17 @@ return {
       name = 'vertices',
       type = 'table',
       description = 'The new set of vertices.'
+    },
+    {
+      name = 'start',
+      type = 'number',
+      default = '1',
+      description = 'The index of the vertex to start replacing at.'
     }
   },
   returns = {},
   notes = [[
-    The number of vertices in the table should not exceed the maximum size of the Mesh.
+    The start index plus the number of vertices in the table should not exceed the maximum size of
+    the Mesh.
   ]]
 }
