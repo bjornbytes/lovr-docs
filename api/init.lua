03df9adca1193279f9809613988fb30525d28aa1
bjorn <tie.372@gmail.com>
2018-04-21 09:26:16 -0700

    Rebuild;

--- api/init.lua
+++ api/init.lua
@@ -2117,9 +2117,12 @@ return {
         {
           name = "isDirectory",
           summary = "Check whether a path is a directory.",
-          description = "Check if a path is a directory.",
+          description = "Check if a path exists and is a directory.",
           key = "lovr.filesystem.isDirectory",
           module = "lovr.filesystem",
+          related = {
+            "lovr.filesystem.isFile"
+          },
           variants = {
             {
               arguments = {
@@ -2142,9 +2145,12 @@ return {
         {
           name = "isFile",
           summary = "Check whether a path is a file.",
-          description = "Check if a path is a file.",
+          description = "Check if a path exists and is a file.",
           key = "lovr.filesystem.isFile",
           module = "lovr.filesystem",
+          related = {
+            "lovr.filesystem.isDirectory"
+          },
           variants = {
             {
               arguments = {
