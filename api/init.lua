0a866af09261f3919d8fbb72d8309ebac595bb9e
bjorn <tie.372@gmail.com>
2018-05-08 00:01:28 -0700

    Type info for function arguments/returns;

--- api/init.lua
+++ api/init.lua
@@ -1774,7 +1774,9 @@ return {
                 {
                   name = "iterator",
                   type = "function",
-                  description = "The iterator function, usable in a for loop."
+                  description = "The iterator function, usable in a for loop.",
+                  arguments = {},
+                  returns = {}
                 }
               }
             }
@@ -2263,7 +2265,19 @@ return {
                 {
                   name = "chunk",
                   type = "function",
-                  description = "The runnable chunk."
+                  description = "The runnable chunk.",
+                  arguments = {
+                    {
+                      name = "...",
+                      type = "*"
+                    }
+                  },
+                  returns = {
+                    {
+                      name = "...",
+                      type = "*"
+                    }
+                  }
                 }
               }
             }
@@ -6760,7 +6774,14 @@ return {
                 {
                   name = "callback",
                   type = "function",
-                  description = "The function used to render.  It will be passed a string representing the current eye that is being rendered to, either \"left\" or \"right\"."
+                  description = "The function used to render.  It will be passed a string representing the current eye that is being rendered to, either \"left\" or \"right\".",
+                  arguments = {
+                    {
+                      name = "eye",
+                      type = "HeadsetEye"
+                    }
+                  },
+                  returns = {}
                 }
               },
               returns = {}
