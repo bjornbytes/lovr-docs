6e8edccf9b4acd13dcf8bde2be3847bf50766f0a
bjorn <tie.372@gmail.com>
2018-06-10 00:25:05 -0700

    Fix redundant lovr.filesystem.setIdentity related;

--- api/init.lua
+++ api/init.lua
@@ -2417,9 +2417,6 @@ return {
           description = "Set the name of the save directory.",
           key = "lovr.filesystem.setIdentity",
           module = "lovr.filesystem",
-          related = {
-            "lovr.filesystem.getIdentity"
-          },
           variants = {
             {
               arguments = {
