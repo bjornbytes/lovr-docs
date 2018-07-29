3fbefd5d0317c0c78516461062228012657c9f60
bjorn <tie.372@gmail.com>
2018-07-28 18:42:35 -0700

    Disable mipmaps on Panorama example;

--- examples/Panorama/main.lua
+++ examples/Panorama/main.lua
@@ -1,7 +1,7 @@
 local skybox
 
 function lovr.load()
-  skybox = lovr.graphics.newTexture('equirectangular.jpg')
+  skybox = lovr.graphics.newTexture('equirectangular.jpg', { mipmaps = false })
 end
 
 function lovr.draw()
