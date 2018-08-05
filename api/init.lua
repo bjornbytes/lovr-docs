3b00d54af329ff6114a27a19c077833c614434a7
bjorn <tie.372@gmail.com>
2018-08-05 11:22:43 -0700

    Channel fix;

--- api/init.lua
+++ api/init.lua
@@ -18837,7 +18837,7 @@ return {
         {
           name = "Channel",
           summary = "A message channel for communicating between threads.",
-          description = "A Channel is an object used to communicate between `Thread` objects.  Channels are obtained by name using `lovr.thread.getChannel`.  Different threads can send messages on the same Channel to communicate with each other.  Messages can be sent and received on a Channel using `Channel:push` and `Channel:pop`.  The following types of data that can be passed through Channels: nil, boolean, number, string, and any LÖVR object.",
+          description = "A Channel is an object used to communicate between `Thread` objects.  Channels are obtained by name using `lovr.thread.getChannel`.  Different threads can send messages on the same Channel to communicate with each other.  Messages can be sent and received on a Channel using `Channel:push` and `Channel:pop`.  The following types of data can be passed through Channels: nil, boolean, number, string, and any LÖVR object.",
           key = "Channel",
           module = "lovr.thread",
           methods = {
