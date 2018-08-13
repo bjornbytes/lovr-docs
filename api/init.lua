1663fa42196d203eebf0ba467cf2609d56f724f0
bjorn <tie.372@gmail.com>
2018-08-13 03:19:08 -0700

    rm AudioStream:seek;

--- api/init.lua
+++ api/init.lua
@@ -2769,25 +2769,6 @@ return {
                   }
                 }
               }
-            },
-            {
-              name = "seek",
-              summary = "Seek the stream to a new position.",
-              description = "Seeks the AudioStream to the specified position.  This will cause any Sources created from the stream to also seek their playback position.",
-              key = "AudioStream:seek",
-              module = "lovr.data",
-              variants = {
-                {
-                  arguments = {
-                    {
-                      name = "time",
-                      type = "number",
-                      description = "The time to seek to, in seconds."
-                    }
-                  },
-                  returns = {}
-                }
-              }
             }
           },
           constructors = {
