1bbbd80a54b47570ae5348e0fca6e8fec7cc07b2
bjorn <tie.372@gmail.com>
2018-08-12 18:15:20 -0700

    require path changes;

--- api/lovr/filesystem/getRequirePath.lua
+++ api/lovr/filesystem/getRequirePath.lua
@@ -6,6 +6,11 @@ return {
     be replaced with the module that is being required.  It is similar to Lua\'s `package.path`
     variable, but the main difference is that the patterns are relative to the save directory and
     the project directory.
+
+    For the C require path, double question marks will be replaced by the name of the module with
+    the operating system's native extension for shared libraries.  For example, if you do
+    `require('lib')` and the C require path is `??`, LÖVR will try to load `lib.dll` if you're on
+    Windows or `lib.so` if you're on Linux.
   ]],
   arguments = {},
   returns = {
@@ -13,9 +18,14 @@ return {
       name = 'path',
       type = 'string',
       description = 'The semicolon separated list of search patterns.'
+    },
+    {
+      name = 'cpath',
+      type = 'string',
+      description = 'The semicolon separated list of search patterns for C libraries.'
     }
   },
   notes = [[
-    The default reqiure path is '?.lua;?/init.lua'.
+    The default reqiure path is '?.lua;?/init.lua'.  The default C require path is '??'.
   ]]
 }
--- api/lovr/filesystem/setRequirePath.lua
+++ api/lovr/filesystem/setRequirePath.lua
@@ -6,16 +6,28 @@ return {
     be replaced with the module that is being required.  It is similar to Lua\'s `package.path`
     variable, but the main difference is that the patterns are relative to the save directory and
     the project directory.
+
+    For the C require path, double question marks will be replaced by the name of the module with
+    the operating system's native extension for shared libraries.  For example, if you do
+    `require('lib')` and the C require path is `??`, LÖVR will try to load `lib.dll` if you're on
+    Windows or `lib.so` if you're on Linux.
   ]],
   arguments = {
     {
       name = 'path',
       type = 'string',
-      description = 'The semicolon separated list of search patterns.'
+      default = 'nil',
+      description = 'An optional semicolon separated list of search patterns.'
+    },
+    {
+      name = 'cpath',
+      type = 'string',
+      default = 'nil',
+      description = 'An optional semicolon separated list of search patterns for C libraries.'
     }
   },
   returns = {},
   notes = [[
-    The default reqiure path is '?.lua;?/init.lua'.
+    The default reqiure path is '?.lua;?/init.lua'.  The default C require path is '??'.
   ]]
 }
