9468e72564d4c3dab3cc030cdbff75ee62932f3b
bjorn <tie.372@gmail.com>
2018-08-25 17:26:29 -0700

    Clarify touchpad directions;

--- api/init.lua
+++ api/init.lua
@@ -11626,11 +11626,11 @@ return {
             },
             {
               name = "touchx",
-              description = "The x axis of the touchpad."
+              description = "The x axis of the touchpad.  Positive values are to the right."
             },
             {
               name = "touchy",
-              description = "The y axis of the touchpad."
+              description = "The y axis of the touchpad.  Positive values are upwards."
             }
           }
         },
