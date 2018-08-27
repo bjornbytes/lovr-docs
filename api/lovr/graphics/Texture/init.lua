343d47ce1bc61b5f3c820fe99203555ffece2422
bjorn <tie.372@gmail.com>
2018-08-27 09:11:59 -0700

    Update supported image formats;

--- api/lovr/graphics/Texture/init.lua
+++ api/lovr/graphics/Texture/init.lua
@@ -1,10 +1,10 @@
 return {
   summary = 'An image that can be applied to Materials.',
   description = [[
-    A Texture is an image that can be applied to `Material`s.  Supported file formats
-    include `.png`, `.jpg`, `.tga`, and `.bmp`.  Additionally, three compressed formats are
-    supported: DXT1, DXT3, and DXT5 (all have the `.dds` extension).  Compressed textures are
-    generally recommended as they use less video memory and usually improve performance.
+    A Texture is an image that can be applied to `Material`s.  The supported file formats are
+    `.png`, `.jpg`, and `.hdr`.  Additionally, three compressed formats are supported: DXT1, DXT3,
+    and DXT5 (all have the `.dds` extension).  Compressed textures are generally recommended as they
+    use less video memory and usually improve performance.
   ]],
   constructor = 'lovr.graphics.newTexture'
 }
