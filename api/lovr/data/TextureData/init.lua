2381c1ee5a55752736bd4f64de602a193487fc40
bjorn <tie.372@gmail.com>
2018-09-06 13:50:34 -0700

    Fix note about RGBA formats;

--- api/lovr/data/TextureData/init.lua
+++ api/lovr/data/TextureData/init.lua
@@ -1,8 +1,9 @@
 return {
   summary = 'An object that stores pixel data for Textures.',
   description = [[
-    A TextureData stores raw pixel info for `Texture`s.  It has a width and a height.  Currently,
-    all TextureData is in the RGBA format.
+    A TextureData stores raw 2D pixel info for `Texture`s.  It has a width, height, and format.  The
+    TextureData can be initialized with the contents of an image file or it can be created with
+    uninitialized contents.  The supported image formats are `png`, `jpg`, `hdr`, and `dds`.
 
     Usually you can just use Textures, but TextureData can be useful if you want to manipulate
     individual pixels or load Textures in a background thread.
