00f72545a21b45d75878952c4953590127dc1b6c
bjorn <tie.372@gmail.com>
2018-09-06 15:18:21 -0700

    Add anisotropy system limit;

--- api/init.lua
+++ api/init.lua
@@ -4472,6 +4472,11 @@ return {
                       name = "texturemsaa",
                       type = "number",
                       description = "The maximum MSAA value supported by `lovr.graphics.newTexture`."
+                    },
+                    {
+                      name = "anisotropy",
+                      type = "number",
+                      description = "The maximum anisotropy value supported by `Texture:setFilter`."
                     }
                   }
                 }
