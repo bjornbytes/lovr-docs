346351cda86c2013273ba6a8864fd82c2c79c3bd
bjorn <tie.372@gmail.com>
2018-09-06 14:47:42 -0700

    Fix getOS;

--- api/init.lua
+++ api/init.lua
@@ -422,7 +422,7 @@ return {
                 {
                   name = "os",
                   type = "string",
-                  description = "Either \"windows\" or \"macOS\"."
+                  description = "Either \"Windows\", \"macOS\", or \"Web\"."
                 }
               }
             }
