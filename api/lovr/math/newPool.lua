5d77831391452be36deb9b646693a235cb9089f9
bjorn <tie.372@gmail.com>
2019-02-15 09:26:00 -0800

    Fix Pool resizable;

--- api/lovr/math/newPool.lua
+++ api/lovr/math/newPool.lua
@@ -1,28 +1,15 @@
 return {
   summary = 'Create a new Pool.',
   description = [[
-    Creates a new `Pool`, used for allocating vector and matrix objects efficiently.
-
-    Note that `lovr.math` has its own Pool that it uses when you call these functions:
-
-    - `lovr.math.vec3`
-    - `lovr.math.quat`
-    - `lovr.math.mat4`
-
-    So it's usually more convenient to use those functions.  Pools are available to use if you have
-    a need for more control over the memory used for your vectors.
+    Creates a new `Pool`, used for allocating vector and matrix objects efficiently.  If you just
+    need vectors, you can use the more convenient functions `lovr.math.vec3`, `lovr.math.quat`, and
+    `lovr.math.mat4`.
   ]],
   arguments = {
     {
       name = 'size',
       type = 'number',
       description = 'The size of the Pool, in bytes.'
-    },
-    {
-      name = 'resizable',
-      type = 'boolean',
-      default = 'false',
-      description = 'Whether or not the Pool will resize itself.'
     }
   },
   returns = {
