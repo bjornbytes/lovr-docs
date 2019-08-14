01c4bbb49b6411f4c1e8d484b0ce7025853dff0d
bjorn <tie.372@gmail.com>
2019-08-13 22:18:56 -0700

    Add note about headset driver semantics;

--- api/lovr/headset/HeadsetDriver.lua
+++ api/lovr/headset/HeadsetDriver.lua
@@ -3,6 +3,13 @@ return {
   description = [[
     These are all of the supported VR APIs that LÖVR can use to power the lovr.headset module.  You
     can change the order of headset drivers using `lovr.conf` to prefer or exclude specific VR APIs.
+
+    At startup, LÖVR searches through the list of drivers in order.  One headset driver will be used
+    for rendering to the VR display, and all supported headset drivers will be used for device
+    input.  The way this works is that when poses or button input is requested, the input drivers
+    are queried (in the order they appear in `conf.lua`) to see if any of them currently have data
+    for the specified device.  The first one that returns data will be used to provide the result.
+    This allows projects to support multiple types of hardware devices.
   ]],
   values = {
     {
