88a73e4430d299e901eec2a32b2c77fd50f18797
bjorn <tie.372@gmail.com>
2019-12-15 15:58:46 -0800

    Fix an an extra word;

--- api/init.lua
+++ api/init.lua
@@ -390,7 +390,7 @@ return {
       name = "threaderror",
       tag = "callbacks",
       summary = "Called when an error occurs in a thread.",
-      description = "The `lovr.threaderror` callback is called whenever an error occurs in a Thread.  It receives a the Thread object where the error occurred and an error message.\n\nThe default implementation of this callback will call `lovr.errhand` with the error.",
+      description = "The `lovr.threaderror` callback is called whenever an error occurs in a Thread.  It receives the Thread object where the error occurred and an error message.\n\nThe default implementation of this callback will call `lovr.errhand` with the error.",
       key = "lovr.threaderror",
       module = "lovr",
       related = {
--- api/lovr/callbacks/threaderror.lua
+++ api/lovr/callbacks/threaderror.lua
@@ -2,8 +2,8 @@ return {
   tag = 'callbacks',
   summary = 'Called when an error occurs in a thread.',
   description = [[
-    The `lovr.threaderror` callback is called whenever an error occurs in a Thread.  It receives a
-    the Thread object where the error occurred and an error message.
+    The `lovr.threaderror` callback is called whenever an error occurs in a Thread.  It receives the
+    Thread object where the error occurred and an error message.
 
     The default implementation of this callback will call `lovr.errhand` with the error.
   ]],
