f5b8378bbf828ecf1d23b78e9892d58f7b3d6750
bjorn <tie.372@gmail.com>
2019-12-31 18:41:47 -0800

    Document getSaveDirectory location;

--- api/lovr/filesystem/getSaveDirectory.lua
+++ api/lovr/filesystem/getSaveDirectory.lua
@@ -9,6 +9,16 @@ return {
       description = 'The absolute path to the save directory.'
     }
   },
+  notes = [[
+    The save directory takes the following form:
+
+    ```
+    <appdata>/LOVR/<identity>
+    ```
+
+    Where `<appdata>` is `lovr.filesystem.getAppdataDirectory` and `<identity>` is
+    `lovr.filesystem.getIdentity` and can be customized using `lovr.conf`.
+  ]],
   related = {
     'lovr.filesystem.getIdentity',
     'lovr.filesystem.getAppdataDirectory'
