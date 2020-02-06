d95030310656e268c3cb9b8891743955f5f9fd6f
bjorn <tie.372@gmail.com>
2020-02-05 16:59:17 -0800

    Mesh:setVertices(blob);

--- api/lovr/graphics/Mesh/setVertices.lua
+++ api/lovr/graphics/Mesh/setVertices.lua
@@ -2,25 +2,36 @@ return {
   summary = 'Update multiple vertices in the Mesh.',
   description = 'Updates multiple vertices in the Mesh.',
   arguments = {
-    {
-      name = 'vertices',
+    vertices = {
       type = 'table',
       description = 'The new set of vertices.'
     },
-    {
-      name = 'start',
+    blob = {
+      type = 'Blob',
+      description = 'A Blob containing binary vertex data to upload (this is much more efficient).'
+    },
+    start = {
       type = 'number',
       default = '1',
       description = 'The index of the vertex to start replacing at.'
     },
-    {
-      name = 'count',
+    count = {
       type = 'number',
       default = 'nil',
       description = 'The number of vertices to replace.  If nil, all vertices will be used.'
     }
   },
   returns = {},
+  variants = {
+    {
+      arguments = { 'vertices', 'start', 'count' },
+      returns = {}
+    },
+    {
+      arguments = { 'blob', 'start', 'count' },
+      returns = {}
+    }
+  },
   notes = [[
     The start index plus the number of vertices in the table should not exceed the maximum size of
     the Mesh.
