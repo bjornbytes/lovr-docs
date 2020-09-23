4fdb0a8f4a81eb1059ffe7de937978ebfda276eb
bjorn <tie.372@gmail.com>
2020-09-22 18:40:58 -0700

    lovr.headset.animate;

--- api/init.lua
+++ api/init.lua
@@ -14025,6 +14025,44 @@ return {
       },
       functions = {
         {
+          name = "animate",
+          tag = "input",
+          summary = "Animate a model to match its Device input state.",
+          description = "Animates a device model to match its current input state.  The buttons and joysticks on a controller will move as they're pressed/moved and hand models will move to match skeletal input.\n\nThe model should have been created using `lovr.headset.newModel` with the `animated` flag set to `true`.",
+          key = "lovr.headset.animate",
+          module = "lovr.headset",
+          variants = {
+            {
+              arguments = {
+                {
+                  name = "device",
+                  type = "Device",
+                  description = "The device to use for the animation data.",
+                  default = "head"
+                },
+                {
+                  name = "model",
+                  type = "Model",
+                  description = "The model to animate."
+                }
+              },
+              returns = {
+                {
+                  name = "success",
+                  type = "boolean",
+                  description = "Whether the animation was applied successfully to the Model.  If the Model was not compatible or animation data for the device was not available, this will be `false`."
+                }
+              }
+            }
+          },
+          related = {
+            "lovr.headset.newModel",
+            "Model:animate",
+            "Model:pose"
+          },
+          notes = "Currently this function is supported for OpenVR controller models and Oculus hand models.\n\nThis function may animate using node-based animation or skeletal animation.  `Model:hasJoints` can be used on a Model so you know if a Shader with the `animated` ShaderFlag needs to be used to render the results properly.\n\nIt's possible to use models that weren't created with `lovr.headset.newModel` but they need to be set up carefully to have the same structure as the models provided by the headset SDK."
+        },
+        {
           name = "getAngularVelocity",
           tag = "input",
           summary = "Get the angular velocity of a device.",
