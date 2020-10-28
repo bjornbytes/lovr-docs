50323510c919819dcacc3f83018e611bbd13e44f
Ben Ferguson <ferguson.ben86@gmail.com>
2020-10-28 19:30:59 -0400

    Added draw()

--- examples/Optimization/Threads/main.lua
+++ examples/Optimization/Threads/main.lua
@@ -5,21 +5,35 @@ local threadCode = [[
 if not lovr then lovr = require "lovr" end 
 if not lovr.thread then lovr.thread = require "lovr.thread" end
 
-local channel = lovr.thread.getChannel('channel')
+local toThread = lovr.thread.getChannel('to')
+local fromThread = lovr.thread.getChannel('from')
+local messageCount = 0
 
 while true do 
-    local msg = channel:pop()
-    if msg ~= nil then print(msg) end 
+    local msg = toThread:pop()
+    if msg ~= nil then 
+        print(msg) 
+        messageCount = messageCount + 1
+        fromThread:push(messageCount)
+    end 
 end
 ]]
 
 function lovr.load()
     thread = lovr.thread.newThread(threadCode)
-    -- Communicating with a new thread requires a channel
-    channel = lovr.thread.getChannel('channel')
+    -- Communicating with a new thread requires a channel.
+    --To avoid congestion, it's a good idea to have several channels
+    toThread = lovr.thread.getChannel('to')
+    fromThread = lovr.thread.getChannel('from')
+    
     thread:start()
 end
 
 function lovr.update() 
-    channel:push("Hello World")
+    toThread:push("Hello World")
+end
+
+function lovr.draw()
+    local n = fromThread:pop()
+    if n ~= nil then lovr.graphics.print('Messages Sent: ' .. n, 0, 2, -8) end
 end
