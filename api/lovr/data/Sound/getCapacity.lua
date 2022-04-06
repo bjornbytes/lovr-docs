1c817bd3e2f2c1ee4eb44bc323d2cc89aa578aaa
bjorn <mail@bjornbyt.es>
2022-04-06 11:21:14 -0700

    Sound:getCapacity;

---
+++ api/lovr/data/Sound/getCapacity.lua
@@ -0,0 +1,21 @@
+return {
+  summary = 'Get the number of frames that can be written to the Sound.',
+  description = [[
+    Returns the number of frames that can be written to the Sound.  For stream sounds, this is the
+    number of frames that can be written without overwriting existing data.  For normal sounds, this
+    returns the same value as `Sound:getFrameCount`.
+  ]],
+  arguments = {},
+  returns = {
+    {
+      name = 'capacity',
+      type = 'number',
+      description = 'The number of frames that can be written to the Sound.'
+    }
+  },
+  related = {
+    'Sound:getFrameCount',
+    'Sound:getSampleCount',
+    'Source:getDuration'
+  }
+}
