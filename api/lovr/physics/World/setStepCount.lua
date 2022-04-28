f20546911d416d825294d39fce9446dd9522e0f5
unknown <mail@bjornbyt.es>
2022-04-27 20:15:43 -0700

    World:get/setStepCount;

---
+++ api/lovr/physics/World/setStepCount.lua
@@ -0,0 +1,20 @@
+return {
+  tag = 'worldProperties',
+  summary = 'Set the step count of the World.',
+  description = [[
+    Sets the step count of the World.  The step count influences how many steps are taken during a
+    call to `World:update`.  A higher number of steps will be slower, but more accurate.  The
+    default step count is 20.
+  ]],
+  arguments = {
+    {
+      name = 'steps',
+      type = 'number',
+      description = 'The new step count.'
+    }
+  },
+  returns = {},
+  related = {
+    'World:update'
+  }
+}
