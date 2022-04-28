ee27db23145dab3eb80a9d8d20e344803a142595
bjorn <mail@bjornbyt.es>
2022-04-28 15:40:48 -0700

    Clarify stride requirements;

--- api/lovr/graphics/Buffer/getStride.lua
+++ api/lovr/graphics/Buffer/getStride.lua
@@ -10,7 +10,11 @@ return {
     }
   },
   notes = [[
-    // TODO zero-stride and straddled-stride
+    When a Buffer is created, the stride can be set explicitly, otherwise it will be automatically
+    computed based on the fields in the Buffer.
+
+    Strides can not be zero, and can not be smaller than the size of a single item.  To work around
+    this, bind the Buffer as a storage buffer and fetch data from the buffer manually.
   ]],
   related = {
     'Buffer:getSize',
