92902ac090be1f79d0d2533570084571fd64615e
bjorn <mail@bjornbyt.es>
2022-06-02 22:37:57 -0700

    Formatting; Rebuild;

--- api/lovr/headset/getDisplayFrequencies.lua
+++ api/lovr/headset/getDisplayFrequencies.lua
@@ -1,13 +1,20 @@
 return {
-  tag = "headset",
-  summary = "Get the list of refresh rates supported by the headset display.",
-  description = "Returns a table with all the refresh rates supported by the headset display, in Hz.",
+  tag = 'headset',
+  summary = 'Get the list of refresh rates supported by the headset display.',
+  description = [[
+    Returns a table with all the refresh rates supported by the headset display, in Hz.
+  ]],
   arguments = {},
   returns = {
     {
-      name = "frequencies",
-      type = "table",
-      description = "A flat table of the refresh rates supported by the headset display, nil if not supported."
+      name = 'frequencies',
+      type = 'table',
+      description = [[
+        A flat table of the refresh rates supported by the headset display, nil if not supported.
+      ]]
     }
+  },
+  related = {
+    'lovr.headset.setDisplayFrequency'
   }
 }
--- api/lovr/headset/setDisplayFrequency.lua
+++ api/lovr/headset/setDisplayFrequency.lua
@@ -1,21 +1,23 @@
 return {
-  tag = "headset",
-  summary = "Set the display refresh rate.",
-  description = "Sets the display refresh rate, in Hz.",
-  module = "lovr.headset",
+  tag = 'headset',
+  summary = 'Set the display refresh rate.',
+  description = 'Sets the display refresh rate, in Hz.',
   arguments = {
     {
-      name = "frequency",
-      type = "number",
-      description = "The refresh rate to set, in Hz",
+      name = 'frequency',
+      type = 'number',
+      description = 'The new refresh rate, in Hz.',
     }
   },
   returns = {
     {
-      name = "success",
-      type = "boolean",
-      description = "Whether the display refresh rate was successfully set."
+      name = 'success',
+      type = 'boolean',
+      description = 'Whether the display refresh rate was successfully set.'
     }
   },
-  notes = "Changing the display refresh-rate also changes the frequency of lovr.update() and lovr.draw() as they depend on the display frequency."
+  notes = [[
+    Changing the display refresh-rate also changes the frequency of lovr.update() and lovr.draw() as
+    they depend on the display frequency.
+  ]]
 }
