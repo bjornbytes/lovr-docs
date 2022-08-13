8cd7be98c970ce7710188af54b2324386d674f7d
bjorn <mail@bjornbyt.es>
2022-08-12 18:12:49 -0700

    Update SCYTHA;

--- examples/Animation/Playback/main.lua
+++ examples/Animation/Playback/main.lua
@@ -6,17 +6,13 @@
 
 function lovr.load()
   model = lovr.graphics.newModel('scytha/scene.gltf')
-  shader = lovr.graphics.newShader('unlit', {
-    flags = { animated = true }
-  })
 end
 
 function lovr.update(dt)
   model:animate(1, lovr.timer.getTime())
 end
 
-function lovr.draw()
-  lovr.graphics.setShader(shader)
-  model:draw(0, 0, -4, .2)
-  lovr.graphics.setShader()
+function lovr.draw(pass)
+  pass:setShader(shader)
+  pass:draw(model, 0, 0, -4, .2)
 end
