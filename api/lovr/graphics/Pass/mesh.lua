ecbfde3625ffadb96c8a0c8922138ad5e15aea02
bjorn <mail@bjornbyt.es>
2022-09-17 11:12:07 -0700

    Add rainbow triangle examples;

--- api/lovr/graphics/Pass/mesh.lua
+++ api/lovr/graphics/Pass/mesh.lua
@@ -96,5 +96,23 @@ return {
     The active `MeshMode` controls whether the vertices are drawn as points, lines, or triangles.
 
     The active `Material` is applied to the mesh.
+  ]],
+  example = [[
+    function lovr.draw(pass)
+      local vertices = {
+        vec3(  0,  .4, 0), vec4(1, 0, 0, 1),
+        vec3(-.5, -.4, 0), vec4(0, 1, 0, 1),
+        vec3( .5, -.4, 0), vec4(0, 0, 1, 1)
+      }
+
+      local format = {
+        { type = 'vec3', location = 'VertexPosition' },
+        { type = 'vec4', location = 'VertexColor' }
+      }
+
+      local triangle = lovr.graphics.getBuffer(vertices, format)
+
+      pass:mesh(triangle, 0, 1.7, -1)
+    end
   ]]
 }
