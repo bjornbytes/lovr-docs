a1853b0315eab939e241627b2f8d9a66eafaaef9
bjorn <mail@bjornbyt.es>
2022-09-21 22:30:34 -0700

    DefaultShader fixes;

--- api/init.lua
+++ api/init.lua
@@ -16867,7 +16867,7 @@ return {
         {
           name = "DefaultShader",
           summary = "Built-in shaders.",
-          description = "The set of shaders built in to LÖVR.  These can be passed to `Pass:setShader` or `lovr.graphics.newShader` instead of writing GLSL code.  The shaders can be further customized by using the `flags` to change their behavior.  If the active shader is set to `nil`, LÖVR picks one of these shaders to use.",
+          description = "The set of shaders built in to LÖVR.  These can be passed to `Pass:setShader` or `lovr.graphics.newShader` instead of writing GLSL code.  The shaders can be further customized by using the `flags` option to change their behavior.  If the active shader is set to `nil`, LÖVR picks one of these shaders to use.",
           key = "DefaultShader",
           module = "lovr.graphics",
           values = {
@@ -16885,15 +16885,15 @@ return {
             },
             {
               name = "cubemap",
-              description = "Renders cubemaps from a fullscreen triangle."
+              description = "Renders cubemaps."
             },
             {
               name = "equirect",
-              description = "Renders spherical textures from a fullscreen triangle."
+              description = "Renders spherical textures."
             },
             {
               name = "fill",
-              description = "Renders a fullscreen triangle, sampling from the color texture."
+              description = "Renders a fullscreen triangle."
             }
           }
         },
--- api/lovr/graphics/DefaultShader.lua
+++ api/lovr/graphics/DefaultShader.lua
@@ -3,8 +3,8 @@ return {
   description = [[
     The set of shaders built in to LÖVR.  These can be passed to `Pass:setShader` or
     `lovr.graphics.newShader` instead of writing GLSL code.  The shaders can be further customized
-    by using the `flags` to change their behavior.  If the active shader is set to `nil`, LÖVR picks
-    one of these shaders to use.
+    by using the `flags` option to change their behavior.  If the active shader is set to `nil`,
+    LÖVR picks one of these shaders to use.
   ]],
   values = {
     {
@@ -21,15 +21,15 @@ return {
     },
     {
       name = 'cubemap',
-      description = 'Renders cubemaps from a fullscreen triangle.'
+      description = 'Renders cubemaps.'
     },
     {
       name = 'equirect',
-      description = 'Renders spherical textures from a fullscreen triangle.'
+      description = 'Renders spherical textures.'
     },
     {
       name = 'fill',
-      description = 'Renders a fullscreen triangle, sampling from the color texture.'
+      description = 'Renders a fullscreen triangle.'
     }
   }
 }
