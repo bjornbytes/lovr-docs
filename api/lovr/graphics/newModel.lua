842e3ec39d498ddb0f0c37a2f00ca0ef66ac130a
bjorn <mail@bjornbyt.es>
2022-10-03 01:42:31 -0700

    Update newModel notes;

--- api/lovr/graphics/newModel.lua
+++ api/lovr/graphics/newModel.lua
@@ -50,6 +50,22 @@ return {
       returns = { 'model' }
     }
   },
+  notes = [[
+    Currently, the following features are not supported by the model importer:
+
+    - glTF: Morph targets are not supported.
+    - glTF: Only the default scene is loaded.
+    - glTF: Only the default scene is loaded.
+    - glTF: Currently, each skin in a Model can have up to 256 joints.
+    - glTF: Meshes can't appear multiple times in the node hierarchy with different skins, they need
+      to use 1 skin consistently.
+    - glTF: `KHR_texture_transform` is supported, but all textures in a material will use the same
+      transform.
+    - STL: ASCII STL files are not supported.
+
+    Diffuse and emissive textures will be loaded using sRGB encoding, all other textures will be
+    loaded as linear.
+  ]],
   related = {
     'lovr.data.newModelData',
     'Pass:draw'
