74cc0165f79e0cd2932d3fd75a4f501407fe0362
bjorn <mail@bjornbyt.es>
2023-04-16 22:20:43 -0700

    More docs;

---
+++ api/lovr/system/getMouseX.lua
@@ -0,0 +1,22 @@
+return {
+  summary = 'Get the x position of the mouse.',
+  description = 'Returns the x position of the mouse.',
+  arguments = {},
+  returns = {
+    x = {
+      type = 'number',
+      description = 'The x position of the mouse, relative to the top-left of the window.'
+    }
+  },
+  variants = {
+    {
+      arguments = {},
+      returns = { 'x' }
+    }
+  },
+  related = {
+    'lovr.system.getMouseY',
+    'lovr.system.getMousePosition',
+    'lovr.mousemoved'
+  }
+}
---
+++ api/lovr/system/getMouseY.lua
@@ -0,0 +1,22 @@
+return {
+  summary = 'Get the y position of the mouse.',
+  description = 'Returns the y position of the mouse.',
+  arguments = {},
+  returns = {
+    y = {
+      type = 'number',
+      description = 'The y position of the mouse, relative to the top-left of the window.'
+    }
+  },
+  variants = {
+    {
+      arguments = {},
+      returns = { 'y' }
+    }
+  },
+  related = {
+    'lovr.system.getMouseX',
+    'lovr.system.getMousePosition',
+    'lovr.mousemoved'
+  }
+}
