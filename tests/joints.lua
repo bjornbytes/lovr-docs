e28fc959fcec7a22f5d02aae1bba23734a798be7
bjorn <mail@bjornbyt.es>
2023-09-13 15:25:38 -0700

    Add file for joint tests;

---
+++ tests/joints.lua
@@ -0,0 +1,11 @@
+function lovr.load()
+  local world = lovr.physics.newWorld()
+  local a, b = world:newCollider(), world:newCollider()
+
+  local joint = lovr.physics.newDistanceJoint(a, b)
+  local anchor1 = vec3(-1, 1, 2)
+  local anchor2 = vec3(1, 2, 0)
+  joint:setAnchors(anchor1, anchor2)
+  assert(vec3(select(1, joint:getAnchors())):equals(anchor1))
+  assert(vec3(select(4, joint:getAnchors())):equals(anchor2))
+end
