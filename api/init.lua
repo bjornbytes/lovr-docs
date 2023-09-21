73ca202e71e10c6c55e2af2c4112caa51118f6c4
bjorn <mail@bjornbyt.es>
2023-09-21 00:59:18 -0700

    rm :type hack;

--- api/init.lua
+++ api/init.lua
@@ -946,7 +946,7 @@ return {
         {
           name = "Object",
           summary = "The base object.",
-          description = "The superclass of all LÖVR objects.\n\nIn addition to the methods here, all objects have a `__tostring` metamethod that returns the name of the object's type.  So `tostring(object) == 'Blob'` will check if a LÖVR object is a Blob.",
+          description = "The superclass of all LÖVR objects.  All objects have these methods.",
           key = "Object",
           module = "lovr",
           methods = {
