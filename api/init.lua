397ba69d6edef06dd05089c8acfd6d4b0a412db9
bjorn <mail@bjornbyt.es>
2023-10-17 09:54:18 -0700

    Clarify key repeat behavior;

--- api/init.lua
+++ api/init.lua
@@ -38888,10 +38888,10 @@ return {
           name = "setKeyRepeat",
           tag = "system-keyboard",
           summary = "Enable or disable key repeat.",
-          description = "Enables or disables key repeat.",
+          description = "Enables or disables key repeat.  Key repeat affects whether `lovr.keypressed` will be fired multiple times while a key is held down.  The `repeat` parameter of the callback can be used to detect whether a key press comes from a \"repeat\" or not.",
           key = "lovr.system.setKeyRepeat",
           module = "lovr.system",
-          notes = "Key repeat is disabled by default.",
+          notes = "Key repeat is disabled by default.  `lovr.textinput` is not affected by key repeat.",
           related = {
             "lovr.keypressed"
           },
