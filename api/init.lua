3bbf419817838b6b55fa7463d626a34bb27abb7b
bjorn <mail@bjornbyt.es>
2024-03-20 09:08:08 -0700

    Fix typo in newSampler;

--- api/init.lua
+++ api/init.lua
@@ -12449,7 +12449,7 @@ return {
                         },
                         {
                           name = "[3]",
-                          type = "FilterMode",
+                          type = "WrapMod",
                           description = "The \"z\" wrap mode for 3D textures."
                         }
                       }
