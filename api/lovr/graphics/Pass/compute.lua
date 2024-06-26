187eaa58cc3781cc82a049f1a0b1ef873f324928
bjorn <mail@bjornbyt.es>
2024-06-26 10:54:06 -0700

    Fix Pass:compute indentation;

--- api/lovr/graphics/Pass/compute.lua
+++ api/lovr/graphics/Pass/compute.lua
@@ -9,33 +9,33 @@ return {
     control the order.
   ]],
   arguments = {
-     x = {
-       type = 'number',
-       default = '1',
-       description = 'The number of workgroups to dispatch in the x dimension.'
-     },
-     y = {
-       type = 'number',
-       default = '1',
-       description = 'The number of workgroups to dispatch in the y dimension.'
-     },
-     z = {
-       type = 'number',
-       default = '1',
-       description = 'The number of workgroups to dispatch in the z dimension.'
-     },
-     buffer = {
-       type = 'Buffer',
-       description = [[
-         A Buffer object containing the x, y, and z workgroup counts, stored as 4 byte unsigned
-         integers.
-       ]]
-     },
-     offset = {
-       type = 'number',
-       default = '0',
-       description = 'The byte offset to read the workgroup counts from in the Buffer.'
-     }
+    x = {
+      type = 'number',
+      default = '1',
+      description = 'The number of workgroups to dispatch in the x dimension.'
+    },
+    y = {
+      type = 'number',
+      default = '1',
+      description = 'The number of workgroups to dispatch in the y dimension.'
+    },
+    z = {
+      type = 'number',
+      default = '1',
+      description = 'The number of workgroups to dispatch in the z dimension.'
+    },
+    buffer = {
+      type = 'Buffer',
+      description = [[
+        A Buffer object containing the x, y, and z workgroup counts, stored as 4 byte unsigned
+        integers.
+      ]]
+    },
+    offset = {
+      type = 'number',
+      default = '0',
+      description = 'The byte offset to read the workgroup counts from in the Buffer.'
+    }
   },
   returns = {},
   variants = {
