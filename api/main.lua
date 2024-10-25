a3ca4656a2903474a1c07e1ef12f170194e0c71c
bjorn <mail@bjornbyt.es>
2024-10-24 17:07:58 -0700

    Warn for missing module functions;

--- api/main.lua
+++ api/main.lua
@@ -438,6 +438,12 @@ local function validateModule(module)
     end
   end
 
+  for name in pairs(t or {}) do
+    if not lookup[module.key .. '.' .. name] then
+      warn('Missing docs for %s', module.name .. '.' .. name)
+    end
+  end
+
   for _, fn in ipairs(module.enums) do
     validateEnum(fn)
   end
