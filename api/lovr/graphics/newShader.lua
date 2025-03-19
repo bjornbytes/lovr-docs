f6885a558344c2a1a36edf4144416875577f1113
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2025-03-19 12:36:18 +0700

    Update documentation.
    Fixed a small mistake.

--- api/lovr/graphics/newShader.lua
+++ api/lovr/graphics/newShader.lua
@@ -7,7 +7,7 @@ return {
     Shader code is usually written in GLSL and compiled to SPIR-V bytecode.  SPIR-V is faster to
     load but requires a build step.  Either form can be used to create a shader.
 
-    By default, the provided shader code is expected to implement a `void lovrmain() { ... }`
+    By default, the provided shader code is expected to implement a `vec4 lovrmain() { ... }`
     function that is called for each vertex or fragment.  If the `raw` option is set to `true`, the
     code is treated as a raw shader and the `lovrmain` function is not required. In this case, the
     shader code is expected to implement its own `main` function.
