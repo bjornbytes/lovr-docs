692ec8b017cb4d4cb87383f78037d70cd0705463
bjorn <mail@bjornbyt.es>
2025-05-10 12:25:47 -0700

    Add clipping example;

---
+++ examples/Effects/Clipping/main.lua
@@ -0,0 +1,18 @@
+function lovr.load()
+  shader = lovr.graphics.newShader([[
+    vec4 lovrmain() {
+      vec3 planePosition = vec3(0, 1.7, 0);
+      vec3 planeNormal = vec3(0, -1, 0);
+
+      gl_ClipDistance[0] = dot(PositionWorld - planePosition, planeNormal);
+
+      return DefaultPosition;
+    }
+  ]], 'normal')
+end
+
+function lovr.draw(pass)
+  pass:setShader(shader)
+  pass:monkey(0, 1.7, -1, .5, lovr.timer.getTime(), 1, 1, 0)
+end
+
