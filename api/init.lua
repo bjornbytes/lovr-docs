a51613d88da026fb8de783cbe92c5465ff5458f2
bjorn <mail@bjornbyt.es>
2025-09-16 19:25:19 -0700

    Add missing "none" BlendMode;

--- api/init.lua
+++ api/init.lua
@@ -10664,6 +10664,10 @@ return {
             {
               name = "screen",
               description = "The opposite of multiply: the pixel colors are inverted, multiplied, and inverted again, producing a lightening effect."
+            },
+            {
+              name = "none",
+              description = "The incoming colors will replace the existing colors.  This is the same as using a blend mode of `nil`."
             }
           }
         },
