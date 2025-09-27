411463ea1ee5ff222a1d86ab61b3c3a2004974a3
bjorn <mail@bjornbyt.es>
2025-09-27 09:15:40 -0700

    Slight clarification to Sound:getCapacity;

--- api/init.lua
+++ api/init.lua
@@ -7482,7 +7482,7 @@ return {
             {
               name = "getCapacity",
               summary = "Get the number of frames that can be written to the Sound.",
-              description = "Returns the number of frames that can be written to the Sound.  For stream sounds, this is the number of frames that can be written without overwriting existing data.  For normal sounds, this returns the same value as `Sound:getFrameCount`.",
+              description = "Returns the number of frames that can be written to the Sound.  For stream sounds, this is the number of frames that can currently be written without overwriting existing data.  For normal sounds, this returns the same value as `Sound:getFrameCount`.",
               key = "Sound:getCapacity",
               module = "lovr.data",
               related = {
