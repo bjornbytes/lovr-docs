ea4c41c6063c57ccf97443be09e01b5087d219e7
bjorn <mail@bjornbyt.es>
2025-11-24 11:49:26 -0800

    rm outdated notes about fixed timestep;

--- api/lovr/physics/World/update.lua
+++ api/lovr/physics/World/update.lua
@@ -18,16 +18,6 @@ return {
     }
   },
   notes = [[
-    By default, the World updates at a fixed timestep.  This means that the physics simulation will
-    always update with a constant rate, for example 60 "ticks" per second.  This improves stability
-    of the simulation and decouples physics from rendering.  Collider poses are automatically
-    interpolated between the two most recent ticks, ensuring smooth movement even if the tick rate
-    is lower than the rendering rate.
-
-    Fixed timestep can be disabled by setting the `tickRate` option to 0 in `lovr.physics.newWorld`.
-    This will use a variable timestep where the `dt` value passed to this function will be applied
-    directly to the physics simulation.
-
     This function must be called from the last thread that called `World:setCallbacks`.  If no
     callbacks are set, then this can be called from any thread.
   ]],
