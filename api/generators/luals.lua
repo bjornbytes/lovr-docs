c2d480f35a1bd2a31d88edfeeca893c5a5d44056
Nelson Lopez <38925212+darltrash@users.noreply.github.com>
2025-12-30 01:06:14 -0300

    Get rid of unnecessary keyword lookup

--- api/generators/luals.lua
+++ api/generators/luals.lua
@@ -1,29 +1,5 @@
 local DOCS_URL = "https://lovr.org/docs/"
 
-local KEYWORD_LOOKUP = {
-    ["and"] = true,
-    ["break"] = true,
-    ["do"] = true,
-    ["else"] = true,
-    ["elseif"] = true,
-    ["end"] = true,
-    ["false"] = true,
-    ["for"] = true,
-    ["function"] = true,
-    ["if"] = true,
-    ["in"] = true,
-    ["local"] = true,
-    ["nil"] = true,
-    ["not"] = true,
-    ["or"] = true,
-    ["repeat"] = true,
-    ["return"] = true,
-    ["then"] = true,
-    ["true"] = true,
-    ["until"] = true,
-    ["while"] = true,
-}
-
 local OPERATOR_LOOKUP = {
     ["add"] = "add",
     ["sub"] = "sub",
@@ -153,10 +129,8 @@ local function writeOperator(func, f)
 end
 
 local function handleParam(name)
-    if name:sub(1, 3) == "..." then  -- ...something -> ...
+    if name:sub(1, 3) == "..." then -- ...something -> ...
         return "..."
-    elseif KEYWORD_LOOKUP[name] then -- return -> return_
-        return name .. "_"
     end
     return name -- a -> a
 end
