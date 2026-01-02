f22460fc19dcad35f1a6b676cf9f35c0dbfc8c11
John Dodis <jfdodis@yahoo.com>
2026-01-02 06:12:53 +0200

    Update keypressed.lua with key repeat notes

--- api/lovr/callbacks/keypressed.lua
+++ api/lovr/callbacks/keypressed.lua
@@ -23,6 +23,11 @@ return {
       returns = {}
     }
   },
+  notes = [[
+    By default this will be fired only once while a key is held down. This can be changed to fire
+    multiple times by setting `lovr.system.setKeyRepeat` to true. In that case `isrepeat` will become
+    true whenever the event is the result of a key repeat and false on a single press.
+  ]],
   related = {
     'lovr.system.wasKeyPressed',
     'lovr.keyreleased',
