7f6bba4d2cc0d984f96f035e337f6f823e4f9495
bjorn <mail@bjornbyt.es>
2026-03-23 14:21:42 -0700

    Source:get/setAbsorption;

---
+++ api/lovr/audio/Source/getAbsorption.lua
@@ -0,0 +1,42 @@
+return {
+  summary = 'Get the absorption coefficients of the Source.',
+  description = [[
+    Returns the absorption coefficients of the Source.  Absorption allows for simulating the decay
+    of audio as it moves through a medium, with different frequencies decaying at different rates.
+    For example, in air, distant sounds usually sound more muffled because higher frequencies decay
+    faster than lower frequencies.
+  ]],
+  arguments = {},
+  returns = {
+    low = {
+      type = 'number',
+      description = 'The amount of absorption for low frequencies (400Hz).'
+    },
+    mid = {
+      type = 'number',
+      description = 'The amount of absorption for midrange frequencies (2.5KHz).'
+    },
+    high = {
+      type = 'number',
+      description = 'The amount of absorption for high frequencies (15KHz).'
+    }
+  },
+  variants = {
+    {
+      arguments = {},
+      returns = { 'low', 'mid', 'high' }
+    }
+  },
+  notes = [[
+    When a Source is created, its absorption coefficients are all zero (no absorption).
+
+    The formula used for the volume factor of each band is `math.exp(-absorption * distance)`.
+
+    The absorption coefficients of air are `.0002`, `.0017`, and `.0182`.
+  ]],
+  related = {
+    'Source:getFalloff',
+    'Source:setFalloff',
+    'AudioMaterial'
+  }
+}
---
+++ api/lovr/audio/Source/setAbsorption.lua
@@ -0,0 +1,52 @@
+return {
+  summary = 'Set the absorption coefficients of the Source.',
+  description = [[
+    Sets the absorption coefficients of the Source.  Absorption allows for simulating the decay of
+    audio as it moves through a medium, with different frequencies decaying at different rates. For
+    example, in air, distant sounds usually sound more muffled because higher frequencies decay
+    faster than lower frequencies.
+  ]],
+  arguments = {
+    low = {
+      type = 'number',
+      description = 'The amount of absorption for low frequencies (400Hz).'
+    },
+    mid = {
+      type = 'number',
+      description = 'The amount of absorption for midrange frequencies (2.5KHz).'
+    },
+    high = {
+      type = 'number',
+      description = 'The amount of absorption for high frequencies (15KHz).'
+    },
+    enable = {
+      type = 'boolean',
+      description = [[
+        Whether absorption should be enabled.  False will set the coefficients to zero, `true`
+        will use the absorption coefficients of air (see notes).
+      ]]
+    }
+  },
+  returns = {},
+  variants = {
+    {
+      arguments = { 'low', 'mid', 'high' },
+      returns = {}
+    },
+    {
+      arguments = { 'enable' },
+      returns = {}
+    }
+  },
+  notes = [[
+    When a Source is created, its absorption coefficients are all zero (no absorption).
+
+    The formula used for the volume factor of each band is `math.exp(-absorption * distance)`.
+
+    The absorption coefficients of air are `.0002`, `.0017`, and `.0182`.
+  ]],
+  related = {
+    'Source:getFalloff',
+    'Source:setFalloff'
+  }
+}
