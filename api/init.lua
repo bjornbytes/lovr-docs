333532e5b3a26904e6df289bf656792887026431
bjorn <mail@bjornbyt.es>
2026-04-13 14:02:06 -0700

    rm Buffer:mapData;

--- api/init.lua
+++ api/init.lua
@@ -14031,43 +14031,6 @@ return {
               }
             },
             {
-              name = "mapData",
-              tag = "buffer-transfer",
-              summary = "Get a writable pointer to the Buffer's memory.",
-              description = "Returns a pointer to GPU memory and schedules a copy from this pointer to the buffer's data. The data in the pointer will replace the data in the buffer.  This is intended for use with the LuaJIT FFI or for passing to C libraries.",
-              key = "Buffer:mapData",
-              module = "lovr.graphics",
-              notes = "The pointer remains valid until the next call to `lovr.graphics.submit`, during which the data in the pointer will be uploaded to the buffer.\n\nThe initial contents of the pointer are undefined.\n\nSpecial care should be taken when writing data:\n\n- Reading data from the pointer will be very slow on some systems, and should be avoided.\n- It is better to write data to the pointer sequentially.  Random access may be slower.",
-              related = {
-                "Blob:getPointer"
-              },
-              variants = {
-                {
-                  arguments = {
-                    {
-                      name = "offset",
-                      type = "number",
-                      description = "A byte offset in the buffer to write to.",
-                      default = "0"
-                    },
-                    {
-                      name = "extent",
-                      type = "number",
-                      description = "The number of bytes to replace.  If nil, writes to the rest of the buffer.",
-                      default = "nil"
-                    }
-                  },
-                  returns = {
-                    {
-                      name = "pointer",
-                      type = "lightuserdata",
-                      description = "A pointer to the Buffer's memory."
-                    }
-                  }
-                }
-              }
-            },
-            {
               name = "newReadback",
               tag = "buffer-transfer",
               summary = "Read back the contents of the Buffer asynchronously.",
