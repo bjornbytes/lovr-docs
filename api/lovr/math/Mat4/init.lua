6d29440141ec9f769faf71e6e2326156d03ebfd0
bjorn <mail@bjornbyt.es>
2026-09-21 15:03:10 -0700

    Add some missing extends/constructors;

--- api/lovr/math/Mat4/init.lua
+++ api/lovr/math/Mat4/init.lua
@@ -1,6 +1,7 @@
 return {
   summary = 'A 4x4 matrix.',
   description = 'A `Mat4` is a math type that holds 16 values in a 4x4 grid.',
+  extends = 'Object',
   constructors = {
     'lovr.math.newMat4',
     'lovr.math.mat4'
--- api/lovr/physics/Contact/init.lua
+++ api/lovr/physics/Contact/init.lua
@@ -1,4 +1,10 @@
 return {
-  summary = 'TODO',
-  description = 'TODO'
+  summary = 'A physics contact between two colliders.',
+  description = [[
+    Contacts hold collision-related state between two colliders.
+
+    They are not created directly, but are reported from the `enter` and `contact` physics
+    callbacks, set with `World:setCallbacks`.
+  ]],
+  extends = 'Object'
 }
